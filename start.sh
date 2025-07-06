90710af5ad54546dba65c710c40f2f043408aab4
rushikeshnarule <75885428+rushikeshnarule@users.noreply.github.com>
2025-07-06 20:59:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Uninstall any old version
+pip uninstall -y python-telegram-bot
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Start the Telegram bot
-python bot.py 
+python bot.py
